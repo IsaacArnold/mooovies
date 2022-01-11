175d21b21fe61b355d018e9f19ade19e927795b7
Isaac Arnold <isaacarnold@icloud.com>
2022-01-12 09:46:24 +1000

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,38 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
-  })
-}
+  });
+};
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const fetch = require("node-fetch");
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.MOVIE_API_V3}`
+  );
+
+  const data = await response.json();
+  console.log(data);
+
+  data.results.forEach((item) => {
+    createNode({
+      ...item,
+      id: item.id.toString(),
+      internal: {
+        type: "TrendingTVShows",
+        contentDigest: createContentDigest(item),
+      },
+    });
+  });
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,45 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { graphql, Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const {
+    allTrendingTvShows: { nodes },
+  } = data;
+  console.log(nodes);
 
-export default IndexPage
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        <h1>Trending TV Shows</h1>
+        {nodes.map((show, index) => (
+          <div key={show.id}>
+            <p>{show.name}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
+              alt={show.name}
+            />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
+export const query = graphql`
+  query TVQuery {
+    allTrendingTvShows {
+      nodes {
+        name
+        id
+        poster_path
+      }
+    }
+  }
+`;
