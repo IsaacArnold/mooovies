fa43fe5eb648c378c25576f29444612624d167e5
Isaac Arnold <isaacarnold@icloud.com>
2022-01-15 16:29:21 +1000

    Removing extra files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-postcss",
-    "gatsby-plugin-extract-image-colors",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,3 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions;
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  });
-};
-
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
